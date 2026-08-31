814e39d7615001608dd27df7fe290752dd6ae12b
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2026-08-31 16:37:47 +0700

    Update and rename locketfix6.js to locketfix7.js

--- Module-Shadowrocket-NMS/Locket Gold/locketfix7.js
+++ Module-Shadowrocket-NMS/Locket Gold/locketfix7.js
@@ -6,26 +6,26 @@ const mapping = {
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
     obj = JSON.parse($response.body);
 
-obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+obj.Attention = "🇻🇳 2/9 SPECIAL EVENT 🇻🇳\n✨ Locket Gold • Chào mừng Quốc Khánh ✨!";
 
 var locket02 = {
     is_sandbox: !1,
     ownership_type: "PURCHASED",
     billing_issues_detected_at: null,
     period_type: "normal",
-    expires_date: "2027-11-25T23:59:59Z",   // ngày hết hạn
+    expires_date: "2027-09-02T23:59:59Z",   // ngày hết hạn
     grace_period_expires_date: null,
     unsubscribe_detected_at: null,
     original_purchase_date: "2027-11-25T00:00:00Z", // ngày mua
-    purchase_date: "2027-11-25T00:00:00Z",          // ngày mua
+    purchase_date: "2027-09-02T00:00:00Z",          // ngày mua
     store: "app_store"
 };
 
 var locket01 = {
     grace_period_expires_date: null,
-    purchase_date: "2027-11-25T00:00:00Z",          // ngày mua
+    purchase_date: "2027-09-02T00:00:00Z",          // ngày mua
     product_identifier: "com.locket02.premium.yearly",
-    expires_date: "2027-11-25T23:59:59Z"            // ngày hết hạn
+    expires_date: "2027-09-02T23:59:59Z"            // ngày hết hạn
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
